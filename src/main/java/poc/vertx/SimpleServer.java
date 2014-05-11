215ec65d725daa7f98daaa3bb1d19a7e08b15143
Marcelo Monteiro <m.monteiro@corp.globo.com>
2014-05-11 15:32:02 -0300

    Client lazy load/initialization

--- src/main/java/poc/vertx/SimpleServer.java
+++ src/main/java/poc/vertx/SimpleServer.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 
 import org.vertx.java.core.Handler;
+import org.vertx.java.core.Vertx;
 import org.vertx.java.core.VoidHandler;
 import org.vertx.java.core.http.HttpClient;
 import org.vertx.java.core.http.HttpClientRequest;
@@ -17,70 +18,201 @@ public class SimpleServer extends Verticle {
 
   public void start() {
 
-      boolean local = false;
       final JsonObject conf = container.config();
+      final Integer timeOutPosEnd = conf.getInteger("timeOutPosEnd", 2000);
 
-      final HashSet<HttpClient> clients = new HashSet<>();
-      final HashSet<HttpClient> clients2 = new HashSet<>();
+      final HashSet<Client> clients = new HashSet<>();
+      final HashSet<Client> clients2 = new HashSet<>();
 
+      boolean local = conf.containsField("local");
       if (local) {
-          clients.add(vertx.createHttpClient().setHost("127.0.0.1").setPort(8081));
-          clients.add(vertx.createHttpClient().setHost("127.0.0.1").setPort(8082));
-          clients2.add(vertx.createHttpClient().setHost("127.0.0.1").setPort(8083));
-          clients2.add(vertx.createHttpClient().setHost("127.0.0.1").setPort(8084));
+          clients.add(new Client().setHost("127.0.0.1").setPort(8081));
+          clients.add(new Client().setHost("127.0.0.1").setPort(8082));
+          clients2.add(new Client().setHost("127.0.0.1").setPort(8083));
+          clients2.add(new Client().setHost("127.0.0.1").setPort(8084));
       } else {
-          clients.add(vertx.createHttpClient().setHost("10.248.92.35").setPort(80));
-          clients.add(vertx.createHttpClient().setHost("10.248.92.36").setPort(80));
-          clients2.add(vertx.createHttpClient().setHost("10.249.51.4").setPort(80));
-          clients2.add(vertx.createHttpClient().setHost("10.249.51.5").setPort(80));
+          clients.add(new Client().setHost("10.248.92.35").setPort(80));
+          clients.add(new Client().setHost("10.248.92.36").setPort(80));
+          clients2.add(new Client().setHost("10.249.51.4").setPort(80));
+          clients2.add(new Client().setHost("10.249.51.5").setPort(80));
       }
 
-     final HashMap<String, HashSet<HttpClient>> vhosts = new HashMap<>();
+     final HashMap<String, HashSet<Client>> vhosts = new HashMap<>();
      vhosts.put("teste.qa02.globoi.com", clients);
      vhosts.put("teste2.qa02.globoi.com", clients2);
 
       vertx.createHttpServer().requestHandler(new Handler<HttpServerRequest>() {
-
         @Override
         public void handle(final HttpServerRequest sRequest) {
-            sRequest.response().setChunked(true);
-
-            String header = sRequest.headers().get("Host").split(":")[0];
-            final HttpClientRequest cReq = ((HttpClient) vhosts.get(header).toArray()[getChoice(clients.size())]).request(sRequest.method(), sRequest.uri(),
-                    new Handler<HttpClientResponse>() {
-
-                public void handle(HttpClientResponse cResponse) {
-
-                    Pump.createPump(cResponse, sRequest.response()).start();
-
-                    cResponse.endHandler(new VoidHandler() {
-                        public void handle() {
-                            sRequest.response().end();
-                            vertx.setTimer(2000L, new Handler<Long>() {
-                                @Override
-                                public void handle(Long arg0) {
-                                    sRequest.response().close();
+            String headerHost = sRequest.headers().get("Host").split(":")[0];
+            final boolean connectionKeepalive = sRequest.headers().contains("Connection") ? 
+                    !sRequest.headers().get("Connection").equals("close") : true;
+            final Client client = ((Client) vhosts.get(headerHost).toArray()[getChoice(clients.size())])
+                    .setKeepAlive(connectionKeepalive)
+                    .setTimeout(timeOutPosEnd);
+            final Handler<HttpClientResponse> handlerHttpClientResponse = new Handler<HttpClientResponse>() {
+                    @Override
+                    public void handle(HttpClientResponse cResponse) {
+                        sRequest.response().headers().set(cResponse.headers());
+                        Pump.createPump(cResponse, sRequest.response()).start();
+                        cResponse.endHandler(new VoidHandler() {
+                            public void handle() {
+                                sRequest.response().end();
+                                if (!connectionKeepalive) {
+                                    vertx.setTimer(timeOutPosEnd, new Handler<Long>() {
+                                        @Override
+                                        public void handle(Long event) {
+                                            try {
+                                                sRequest.response().close();
+                                            } catch (RuntimeException e) {
+                                                System.err.println(e.getMessage());
+                                            }
+                                            client.close();
+                                        }
+                                    });
                                 }
-                            });
-                        }
-                    });
+                            }
+                        });
+                        cResponse.exceptionHandler(new Handler<Throwable>() {
+                            @Override
+                            public void handle(Throwable event) {
+                                System.err.println(event.getMessage());
+                                serverShowErrorAndClose(sRequest, event);
+                            }
+                        });
                 }
-            }).setChunked(true);
-
-            Pump.createPump(sRequest, cReq).start();
-
+            };
+            sRequest.response().setChunked(true);
+            final HttpClientRequest cRequest = client.getClient()
+                    .request(sRequest.method(), sRequest.uri(),handlerHttpClientResponse)
+                    .setChunked(true);
+            cRequest.headers().set(sRequest.headers());
+            Pump.createPump(sRequest, cRequest).start();
+            cRequest.exceptionHandler(new Handler<Throwable>() {
+                @Override
+                public void handle(Throwable event) {
+                    System.err.println(event.getMessage());
+                    serverShowErrorAndClose(sRequest, event);
+                }
+            });
             sRequest.endHandler(new VoidHandler() {
                 public void handle() {
-                  cReq.end();
+                  cRequest.end();
                 }
               });
-
         }
+    })
+    .setTCPKeepAlive(conf.getBoolean("serverTCPKeepAlive",true))
+    .listen(conf.getInteger("port",8080));
+  }
 
-        private int getChoice(int size) {
-            int choice = (int) (Math.random() * (size - Float.MIN_VALUE));
-            return choice;
+  private int getChoice(int size) {
+      int choice = (int) (Math.random() * (size - Float.MIN_VALUE));
+      return choice;
+  }
+
+  private void serverShowErrorAndClose(final HttpServerRequest sRequest, final Throwable event) {
+      if (event.getCause() instanceof java.util.concurrent.TimeoutException) {
+          sRequest.response().setStatusCode(504);
+          sRequest.response().setStatusMessage("Gateway Time-Out");
+      } else {
+          sRequest.response().setStatusCode(502);
+          sRequest.response().setStatusMessage("Bad Gateway");
+      }
+      sRequest.response().end();
+      vertx.setTimer(100L, new Handler<Long>() {
+        @Override
+        public void handle(Long event) {
+            try {
+                sRequest.response().close();
+            } catch (RuntimeException e) {
+                System.err.println(e.getMessage());
+            }
         }
-    }).listen(conf.getInteger("port",8080));
+      });
+
+  }
+
+  private class Client {
+
+      HttpClient client;
+      String host;
+      Integer port;
+      boolean keepalive;
+      Integer timeout;
+
+      public Client() {
+          this.client = null;
+          this.host = "127.0.0.1";
+          this.port = 80;
+          this.keepalive = true;
+          this.timeout = 60000;
+      }
+
+      public String getHost() {
+          return host;
+      }
+
+      public Client setHost(String host) {
+          this.host = host;
+          return this;
+      }
+
+      public Integer getPort() {
+          return port;
+      }
+
+      public Client setPort(Integer port) {
+          this.port = port;
+          return this;
+      }
+
+      public boolean isKeepalive() {
+          return keepalive;
+      }
+
+      public Client setKeepAlive(boolean keepalive) {
+          this.keepalive = keepalive;
+          return this;
+      }
+
+      public Integer getTimeout() {
+          return timeout;
+      }
+
+      public Client setTimeout(Integer timeout) {
+          this.timeout = timeout;
+          return this;
+      }
+
+      // Lazy initialization
+      public HttpClient getClient() {
+          if (client==null) {
+              client = vertx.createHttpClient()
+                  .setKeepAlive(keepalive)
+                  .setTCPKeepAlive(keepalive)
+                  .setConnectTimeout(timeout)
+                  .setHost(host)
+                  .setPort(port);
+              client.exceptionHandler(new Handler<Throwable>() {
+                @Override
+                public void handle(Throwable e) {
+                    System.err.println(e.getMessage());
+                }
+              });
+          }
+          return client;
+      }
+
+      public void close() {
+          if (client!=null) {
+              try {
+                  client.close();
+              } catch (IllegalStateException e) {
+                  // Already closed
+                  System.err.println(e.getMessage());
+              }          }
+          client=null;
+      }
   }
 }
